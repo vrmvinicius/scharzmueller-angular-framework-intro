9ba54e7a682269b469ec2640259d009f2df717ea
vinicius.miglioranca <vinicius.miglioranca@cwi.com.br>
2021-10-01 13:34:56 -0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'scharzmueller-angular-framework-intro';
+  name = 'Vinicius';
+
+  onChangeName(){
+     this.name = "Vinicius Rafael Migliorança";
+  }
 }
